ed756f330e797c4df5371cae2de8c1e8228e3c47
tomitahajime <ponponpon816@gmail.com>
2021-12-05 05:27:27 +0900

    [Add]1-1コメント

--- Java-introduction/src/String01.java
+++ Java-introduction/src/String01.java
@@ -6,15 +6,24 @@ public class String01 {
 		String s1 = "スッキリJava";
 		String s2 = "Java";
 		String s3 = "java";
+
+		// Stringクラスに備わる文字列調査のメソッド
+		// equalsメソッド：内容が等しいか調べる。
 		if (s2.equals(s3)) {
 			System.out.println("s2とs3は等しい");
 		}
+
+		// equalsIgnoreCaseメソッド：大文字、小文字（文字のケース）を区別せず、内容が等しいか調べる。
 		if (s2.equalsIgnoreCase(s3)) {
 			System.out.println("s2とs3はケースを区別しなければ等しい");
 		}
+
+		// length()メソッド：文字列長を調べる。
 		System.out.println("s1の長さは" + s1.length() + "です");
+
+		// isEmpty()メソッド：空文字かを調べる。
 		if (s1.isEmpty()) {
-			System.out.println("s1はから文字です");
+			System.out.println("s1は空文字です");
 		}
 	}
 }
