ce6b8c54af500ad3f0ea37726d44561026dd54f8
tomitahajime <ponponpon816@gmail.com>
2021-12-06 22:52:25 +0900

    [Add]1-3 文字列切り出しメソッドを利用した例

---
+++ Java-introduction/src/String03.java
@@ -0,0 +1,14 @@
+// 第1章 文字列の操作
+// 1-3 文字列切り出しメソッドを利用した例
+
+public class String03 {
+	public static void main(String[] args) {
+		String s1 = "Java programming";
+
+		// Stringクラスに備わる文字列切り出しのメソッド
+		// substring：指定位置から始まる文字列を任意の長さだけ切り出す
+		System.out.println("文字列s1の3文字目以降は" + s1.substring(3)); // a programming
+
+		System.out.println("文字列s1の3〜8文字目は" + s1.substring(3, 8)); // a pro
+	}
+}
