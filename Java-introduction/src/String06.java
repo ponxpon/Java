61d0db47f0b1fa0d3a7f98cc99515c8f752cfc50
tomitahajime <ponponpon816@gmail.com>
2021-12-09 23:04:50 +0900

    [Add]p38 正規表現の活用 1-6 文字列パターンを用いたプレーヤー名のチェック

---
+++ Java-introduction/src/String06.java
@@ -0,0 +1,9 @@
+// 第1章 文字列の操作 正規表現の活用 p38
+// 1-6 文字列パターンを用いたプレーヤー名のチェック
+
+public class String06 {
+	boolean isValidPlayerName(String name) {
+		// matchesメソッド：文字列がパターンに従った形式を満たしている（マッチする）かを照合する
+		return name.matches("[A-Z][A-Z0-9]{7}"); // matches("文字列パターン")
+	}
+}
