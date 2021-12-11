867dd4936268b5220bb87e4c862b1e6aa38088d2
tomitahajime <ponponpon816@gmail.com>
2021-12-12 07:23:18 +0900

    [Add]p45 1.5文字列の書式整形,1-9 キャラクター状態1人分の表示のためのコード

---
+++ Java-introduction/src/String09.java
@@ -0,0 +1,12 @@
+// 第1章 文字列の操作 1.5文字列の書式整形 p45
+// 1-9 キャラクター状態1人分の表示のためのコード
+
+public class String09 {
+	public static void main(String[] args) {
+		final String FORMAT = "%8s %6s 所持金%,5d";
+
+		// format()メソッド：整形した文字列を組み立てる
+		String s = String.format(FORMAT, hero.getName(), hero.getJob(), hero.getGold());
+		System.out.println(s);
+	}
+}
