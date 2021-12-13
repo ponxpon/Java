ce1991fb99bbfbe8c0c8735b52ee1c7eb09ef2b5
tomitahajime <ponponpon816@gmail.com>
2021-12-13 11:10:38 +0900

    [Add]p48 1.7練習問題,練習 1-1

--- Java-introduction/src/String/String10.java
+++ Java-introduction/src/String/String10.java
@@ -1,5 +1,20 @@
+// 第1章 文字列の操作 1.7練習問題 p48
+// 練習 1-1
+
 package String;
 
 public class String10 {
+	public static void main(String[] args) {
+		// 1から100までの整数をカンマで連結した文字列sを生成する
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < 100; i++) {
+			sb.append(i+1).append(",");
+		}
 
+		// 完成した文字列sをカンマで分割し、String配列aに格納
+		String s = sb.toString();
+		String[] a = s.split(",");
+
+		System.out.println(s);
+	}
 }
