d96cf585a9696d24c83973464fba6bcf62c90be6
tomitahajime <ponponpon816@gmail.com>
2022-01-14 06:37:04 +0900

    [Add]p114 4.2.2_toString()をオーバーライドする 4-1 toString()メソッドのオーバーライド,コメント

--- Java-introduction/src/instance/Instance01.java
+++ Java-introduction/src/instance/Instance01.java
@@ -6,6 +6,7 @@ public class Instance01 {
 	private String name;
 	private int hp, mp;
 
+	// toString()メソッド：インスタンスの内容を、人が読んで理解できる文字列で表現したもの（文字列表現）として返す
 	public String toString() {
 		// ObjectクラスのtoStringをオーバーライド
 		return "勇者（名前=" + this.name + "/HP=" + this.hp + "/MP=" + this.mp + ")";
