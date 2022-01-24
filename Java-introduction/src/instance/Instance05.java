de5bf323d4549c436613db840fd2a800ace05fce
tomitahajime <ponponpon816@gmail.com>
2022-01-24 21:36:22 +0900

    [Add]p125 4.5_インスタンスの順序づけ,4.5.1_インスタンスの並び替え,4-6 口座インスタンスの並び替え

---
+++ Java-introduction/src/instance/Instance05.java
@@ -0,0 +1,15 @@
+// 第4章 インスタンスの基本操作 4.5_インスタンスの順序づけ p125 4.5.1_インスタンスの並び替え
+// 4-6 口座インスタンスの並び替え
+
+package instance;
+
+public class Instance05 {
+	public static void main(String[] args) {
+		// List<Account> list = new ArrayList<Account>();
+
+		// これだけで要素が並び替えらえれる
+		// Collections.sort(list);
+		// sortには1つ重要な制約があり、それを意識せずにコードを書くと文法エラーが起きる。
+		// 自然順序が定めてあるクラスであれば、エラーは出ずに並び替えることができる。
+	}
+}
